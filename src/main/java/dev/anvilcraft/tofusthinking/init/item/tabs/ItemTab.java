88eb11d84176276089940e16fe22c046230f0b73
CTicetofu <b1469812303@163.com>
2026-09-25 17:20:50 +0800

    在创造物品栏添加了方块

--- src/main/java/dev/anvilcraft/tofusthinking/init/item/tabs/ItemTab.java
+++ src/main/java/dev/anvilcraft/tofusthinking/init/item/tabs/ItemTab.java
@@ -1,5 +1,6 @@
 package dev.anvilcraft.tofusthinking.init.item.tabs;
 
+import dev.anvilcraft.tofusthinking.init.block.AddonBlocks;
 import dev.anvilcraft.tofusthinking.init.item.AddonItems;
 import dev.anvilcraft.tofusthinking.util.DataClass.EnchantmentKeyInstance;
 import net.minecraft.world.item.enchantment.Enchantments;
@@ -20,5 +21,11 @@ public class ItemTab extends BaseCreativeTab{
         this.acceptFullEnergy(AddonItems.SONIC_BOOM_STAFF);
         this.accept(AddonItems.ORIGINAL_CONDUIT_STAFF);
         this.acceptFullEnergy(AddonItems.ORIGINAL_CONDUIT_STAFF);
+        this.accept(AddonBlocks.ORIGINAL_CONDUIT.asItem());
+        this.accept(AddonBlocks.OVERLOAD_GENERATOR.asItem());
+        this.accept(AddonBlocks.STABLE_PRISMARINE_BRICKS.asItem());
+        this.accept(AddonBlocks.SMART_POWER_CONVERTER.asItem());
+        this.accept(AddonBlocks.SMART_POWER_CONVERTER_EXTREMELY_BIG.asItem());
+        this.accept(AddonBlocks.TOFU_ANVIL.asItem());
     }
 }
